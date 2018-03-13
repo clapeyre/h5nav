6881bbf8bb3f78d534e086af2d245123d15da862
Corentin Lapeyre <lapeyre@cerfacs.fr>
2018-03-13 23:20:52 +0100

    Update version to 0.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="h5nav",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(exclude=['docs']),
     entry_points={
         'console_scripts': [
